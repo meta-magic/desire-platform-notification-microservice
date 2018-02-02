d67d7d30ef8cba2b130d1e2b97d70aa5d7b6bad3
ashujadhav123 <ashutosh.jadhav@metamagic.in>
2018-02-02 12:59:13 +0530

    war configuration.

--- notification-service/src/main/java/com/desire3d/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/desire3d/notification/NotificationServiceApplication.java
@@ -2,6 +2,8 @@ package com.desire3d.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +14,14 @@ import com.desire3d.notification.channel.NotificationChannel;
 @Configuration
 @EnableBinding(NotificationChannel.class)
 @EnableEurekaClient
-public class NotificationServiceApplication {
+public class NotificationServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(NotificationServiceApplication.class);
 	}
 }
