c6cbb87651c0da5ba05f86c78d886c3a9609ea60
ashwiniagre1 <ashwini.agre@metamagic.in>
2018-06-14 17:11:08 +0530

    subject change

--- notification-database/incremental.sh
+++ notification-database/incremental.sh
@@ -5,11 +5,16 @@
 # This script will restore the database setup
 
 FILE_PATH=$(pwd)
+POSTGRES=/usr/lib/postgresql/9.6/bin
+DBNAME=notificationdb
+DBUSER=postgres
 
 echo "Database inremental process started"
 
-sh ./drop.sh
-sh ./create.sh
-sh ./load.sh
+#sh ./drop.sh
+#sh ./create.sh
+#sh ./load.sh
+
+$POSTGRES/psql -d $DBNAME -U $DBUSER -f $FILE_PATH/incremental/TEMPLATE.sql
 
 echo "Database inremental process completed successfully"
---
+++ notification-database/incremental/TEMPLATE.sql
@@ -0,0 +1,2 @@
+UPDATE public."TEMPLATE" set "SUBJECT" = 'Amexio Canvas PE (Personal Edition)'
+where "TEMPLATEID" = 'd5cbc718-c650-440d-a23d-f2185b80f434';
