5ca6927e53d6de711b963767cd2122fd849977ed
ashwiniagre1 <ashwini.agre@metamagic.in>
2018-10-30 19:06:47 +0530

    password changes

--- notification-database/script/create-tablespace.sql
+++ notification-database/script/create-tablespace.sql
@@ -1,2 +1,4 @@
-CREATE TABLESPACE notificationdbdata OWNER notificationdb_user LOCATION '/home/mahesh/platform/postgres-tablespace/notification_database/data';
-CREATE TABLESPACE notificationdbindex OWNER notificationdb_user LOCATION '/home/mahesh/platform/postgres-tablespace/notification_database/index';
+CREATE TABLESPACE notificationdbdata OWNER notificationdb_user LOCATION '/home/ubuntu/Postgres_Structural_Database/notification_tablespace/data';
+CREATE TABLESPACE notificationdbindex OWNER notificationdb_user LOCATION '/home/ubuntu/Postgres_Structural_Database/notification_tablespace/index';
+/notification_tablespace$ 
+
