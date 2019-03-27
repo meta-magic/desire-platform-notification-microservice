2c919187ea3440ddd02fa6a70985a7d71a4c61f5
sagarjadhav968 <sagarjdhv968@gmail.com>
2019-03-27 20:45:20 +0530

    updated

--- notification-service/src/main/java/com/desire3d/notification/MailConfig.java
+++ notification-service/src/main/java/com/desire3d/notification/MailConfig.java
@@ -1,7 +1,7 @@
 package com.desire3d.notification;
 
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.beans.factory.annotation.Value;
 /**
  * Configuration class to read Mail properties
  * 
@@ -25,23 +25,28 @@ public class MailConfig {
 
 	public static final Integer PORT = 587;
 
+	@Value("${mail.smtp.host}")
 	public void setHost(String host) {
-		MailConfig.HOST = System.getenv("MAIL_SMTP_HOST");
+		MailConfig.HOST = host;
 	}
 
+	@Value("${mail.smtp.username}")
 	public void setUsername(String username) {
-		MailConfig.USER_NAME = System.getenv("MAIL_SMTP_USERNAME");
+		MailConfig.USER_NAME = username;
 	}
 
+	@Value("${mail.debug}")
 	public void setDebug(boolean debug) {
-		MailConfig.DEBUG = Boolean.valueOf(System.getenv("MAIL_DEBUG"));
+		MailConfig.DEBUG = debug;
 	}
 
+	@Value("${mail.connectiontimeout}")
 	public void setConnectiontimeout(Integer connectiontimeout) {
-		MailConfig.CONNECTION_TIMEOUT = Integer.valueOf(System.getenv("MAIL_CONNECTIONTIMEOUT"));
+		MailConfig.CONNECTION_TIMEOUT = connectiontimeout;
 	}
 
+	@Value("${mail.timeout}")
 	public void setTimeout(Integer timeout) {
-		MailConfig.SOCKET_TIMEOUT = Integer.valueOf(System.getenv("MAIL_TIMEOUT"));
+		MailConfig.SOCKET_TIMEOUT = timeout;
 	}
 }
